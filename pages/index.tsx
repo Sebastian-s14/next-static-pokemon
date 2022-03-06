739b3d17d05f5b301a3b9274f90106c7e76c2a22
Sebastian <sebastian.sanchez@tecsup.edu.pe>
2022-03-06 13:29:39 -0500

    feat: add nextui, add dark theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
 import type { NextPage } from 'next'
+import { Button } from '@nextui-org/react'
+
+// const Component = () => (
+//     <Button onClick={() => console.log('im a simple button')}>Click me</Button>
+// )
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <hr />
+            {/* <Component /> */}
+            <Button color={'success'} onClick={() => console.log('im a simple button')}>
+                Click me
+            </Button>
+        </div>
+    )
 }
 
 export default Home
