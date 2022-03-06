7a9c1c9247f98f160b5fecb42900d349a3d924e3
Sebastian <sebastian.sanchez@tecsup.edu.pe>
2022-03-06 16:40:58 -0500

    feat: add links in Navbar and add favoritos page

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,5 +1,6 @@
-import { Spacer, Text, useTheme } from '@nextui-org/react'
 import Image from 'next/image'
+import NextLink from 'next/link'
+import { Spacer, Text, useTheme, Link } from '@nextui-org/react'
 
 export const Navbar = () => {
     const { theme } = useTheme()
@@ -20,14 +21,22 @@ export const Navbar = () => {
                 height={70}
                 width={70}
             />
-            <Text color="white" h2>
-                P
-            </Text>
-            <Text color="white" h3>
-                okémon
-            </Text>
+            <NextLink href="/" passHref>
+                <Link>
+                    <Text color="white" h2>
+                        P
+                    </Text>
+                    <Text color="white" h3>
+                        okémon
+                    </Text>
+                </Link>
+            </NextLink>
             <Spacer css={{ flex: 1 }} />
-            <Text color="white">Favoritos</Text>
+            <NextLink href="/favoritos" passHref>
+                <Link>
+                    <Text color="white">Favoritos</Text>
+                </Link>
+            </NextLink>
         </div>
     )
 }
---
+++ pages/favoritos/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+import { Layout } from '../../components/layouts'
+
+const FavoritosPage: NextPage = () => {
+    return (
+        <Layout title="Favoritos">
+            <div>Página de favortios</div>
+        </Layout>
+    )
+}
+
+export default FavoritosPage
